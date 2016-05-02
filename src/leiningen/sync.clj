c32909f8ca629ae1a3d15f83f38efa77b02a32c2
Minh Tuan Nguyen <minhtuan.nguyen@otto.de>
2016-05-03 00:31:32 +0200

    add some hint at the end of process

--- src/leiningen/sync.clj
+++ src/leiningen/sync.clj
@@ -7,13 +7,20 @@
             [clojure.tools.cli :refer [parse-opts]]
             [clojure.string :as str]))
 
-(defn do-update [projects ns {no-test? :notest
-                              reset?   :reset
-                              diff?    :diff
-                              pull?    :pull
-                              push?    :push
-                              status?  :status
-                              commit?  :commit}]
+(defn update-and-test! [projects namespaces]
+  (do (u/run! ns/update-ns-of-projects! projects namespaces)
+      (u/run! ns/test-all projects)
+      (main/info "lein sync" (str/join "," projects) "--diff" "to see changes")
+      (main/info "lein sync" (str/join "," projects) "--commit" "to commit changes")
+      (main/info "lein sync" (str/join "," projects) "--push" "to push unpushed commit")))
+
+(defn do-update [projects namespaces {no-test? :notest
+                                      reset?   :reset
+                                      diff?    :diff
+                                      pull?    :pull
+                                      push?    :push
+                                      status?  :status
+                                      commit?  :commit}]
   (cond
     (true? status?) (u/run! ns/status_all projects)
     (true? push?) (u/run! ns/push-all! projects)
@@ -21,9 +28,8 @@
     (true? commit?) (u/run! ns/commit-all! projects)
     (true? diff?) (u/run! ns/show-all-diff projects)
     (true? reset?) (u/run! ns/reset-all! projects)
-    (true? no-test?) (u/run! ns/update-ns-of-projects! projects ns)
-    :else (do (u/run! ns/update-ns-of-projects! projects ns)
-              (u/run! ns/test-all projects))))
+    (true? no-test?) (u/run! ns/update-ns-of-projects! projects namespaces)
+    :else (update-and-test! projects namespaces)))
 
 (defn one-arg-program [project-description projects options]
   (do-update (u/split projects) (ns/spec-selector project-description) options))
