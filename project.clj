f925a77247ecec96d123852cfa7a0ecbef3b9aa9
Minh Tuan Nguyen <minhtuannguyen2704@gmail.com>
2016-06-01 09:07:40 +0200

    pump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject sync "0.9.9"
+(defproject sync "0.9.10-SNAPSHOT"
   :description "sync is a Leiningen plugin to sync same code base between different clojure projects"
   :url "https://github.com/otto-de/leinsync"
   :license {:name "Eclipse Public License"
