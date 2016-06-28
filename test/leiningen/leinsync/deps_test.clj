1d8338d39df39809c84374c0b483bc206be6278f
minhtuan.nguyen <minhtuan.nguyen@otto.de>
2016-06-28 18:44:16 +0200

    first implementation of deps-list

--- test/leiningen/leinsync/deps_test.clj
+++ test/leiningen/leinsync/deps_test.clj
@@ -3,21 +3,52 @@
             [leiningen.leinsync.deps :as d]))
 
 (deftest ^:unit flat-deps-list
-  (is (= [{:name :dep-1 :k :v-1}
-          {:name :dep-2 :k :v-2}]
+  (is (= {:dep-1 {:k :v-1}
+          :dep-2 {:k :v-2}}
          (d/flat-deps-list :k [[:dep-1 :v-1]
                                [:dep-2 :v-2]]))))
 
-(comment (deftest ^:unit check-deps
-           (is (= [{:name :dep-1 :deps-project-1 :v-1 :deps-project-2 :v-1 :deps-project-3 :v-1}
-                   {:name :dep-2 :deps-project-1 :v-1 :deps-project-3 :v-1}
-                   {:name :dep-3 :deps-project-1 :v-1}
-                   {:name :dep-4 :deps-project-2 :v-1}]
-                  (d/extract-deps-list
-                   {:deps-project-1 {:dependencies [[:dep-1 :v-1]
-                                                    [:dep-2 :v-2]
-                                                    [:dep-3 :v-3]]}
-                    :deps-project-2 {:dependencies [[:dep-1 :v-1]]}
-                    :deps-project-3 {:dependencies [[:dep-1 :v-1]
-                                                    [:dep-2 :v-2]
-                                                    [:dep-4 :v-5]]}})))))
+(deftest ^:unit deps->project
+  (is (= [[:dep-1 {:deps-project-1 :v-1}]
+          [:dep-2 {:deps-project-1 :v-2}]
+          [:dep-3 {:deps-project-1 :v-3}]
+          [:dep-1 {:deps-project-2 :v-1}]
+          [:dep-1 {:deps-project-3 :v-1}]
+          [:dep-2 {:deps-project-3 :v-2}]
+          [:dep-4 {:deps-project-3 :v-5}]]
+         (d/deps->project
+          {:deps-project-1 {:dependencies [[:dep-1 :v-1]
+                                           [:dep-2 :v-2]
+                                           [:dep-3 :v-3]]}
+           :deps-project-2 {:dependencies [[:dep-1 :v-1]]}
+           :deps-project-3 {:dependencies [[:dep-1 :v-1]
+                                           [:dep-2 :v-2]
+                                           [:dep-4 :v-5]]}}))))
+
+(deftest ^:unit merge-deps
+  (is (= {:dep-1 {:deps-project-1 :v-1, :deps-project-2 :v-1, :deps-project-3 :v-1}
+          :dep-2 {:deps-project-1 :v-2, :deps-project-3 :v-2}
+          :dep-3 {:deps-project-1 :v-3}
+          :dep-4 {:deps-project-3 :v-5}}
+         (d/merge-deps
+          [[:dep-1 {:deps-project-1 :v-1}]
+           [:dep-2 {:deps-project-1 :v-2}]
+           [:dep-3 {:deps-project-1 :v-3}]
+           [:dep-1 {:deps-project-2 :v-1}]
+           [:dep-1 {:deps-project-3 :v-1}]
+           [:dep-2 {:deps-project-3 :v-2}]
+           [:dep-4 {:deps-project-3 :v-5}]]))))
+
+(deftest ^:unit merge-deps
+  (is (= [{:deps-project-1 :v-1
+           :deps-project-2 :v-1
+           :deps-project-3 :v-1
+           :name           :dep-1}
+          {:deps-project-1 :v-2, :deps-project-3 :v-2, :name :dep-2}
+          {:deps-project-1 :v-3, :name :dep-3}
+          {:deps-project-3 :v-5, :name :dep-4}]
+         (d/pretty-print-structure
+          {:dep-1 {:deps-project-1 :v-1, :deps-project-2 :v-1, :deps-project-3 :v-1}
+           :dep-2 {:deps-project-1 :v-2, :deps-project-3 :v-2}
+           :dep-3 {:deps-project-1 :v-3}
+           :dep-4 {:deps-project-3 :v-5}}))))
