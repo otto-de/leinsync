6823f6833c3fbe0dac11915d5bfd1018178af3b2
Minh Tuan Nguyen <minhtuannguyen2704@gmail.com>
2016-07-06 21:46:56 +0200

    clean up pretty print

--- src/leiningen/leinsync/table_pretty_print.clj
+++ src/leiningen/leinsync/table_pretty_print.clj
@@ -1,40 +1,44 @@
 (ns leiningen.leinsync.table-pretty-print
-  (:require [leiningen.core.main :as m]
-            [clojure.string :as str]))
+  (:require [leiningen.core.main :as m]))
 
-(defn print-table
-  ([rows with-extra-seperator-line log-fn]
-   (let [ks (->> rows
-                 (map keys)
-                 (distinct)
-                 (reduce concat)
-                 (distinct))]
-     (print-table ks rows with-extra-seperator-line log-fn)))
-  ([ks rows with-extra-seperator-line log-fn]
-   (when (seq rows)
-     (let [widths (map
-                   (fn [k]
-                     (apply max (count (str k)) (map #(count (str (get % k))) rows)))
-                   ks)
-           spacers (map #(apply str (repeat % "-")) widths)
-           fmts (map #(str "%" % "s") widths)
-           fmt-row (fn [leader divider trailer row]
-                     (str leader
-                          (apply str (interpose divider
-                                                (for [[col fmt] (map vector (map #(get row %) ks) fmts)]
-                                                  (format fmt (str col)))))
-                          trailer))]
-       (log-fn "\n")
-       (log-fn (fmt-row "| " " | " " |" (zipmap ks ks)))
-       (log-fn (fmt-row "|-" "-+-" "-|" (zipmap ks spacers)))
-       (doseq [row rows]
-         (log-fn (fmt-row "| " " | " " |" row))
-         (if with-extra-seperator-line
-           (log-fn (fmt-row "|-" "---" "-|" (zipmap ks spacers)))))
-       (log-fn "\n")))))
+(defn get-formatter [keys widths]
+  (fn [left middle right row]
+    (str left
+         (apply str
+                (interpose middle
+                           (for [[col fmt]
+                                 (map vector
+                                      (map #(get row %) keys)
+                                      (map #(str "%" % "s") widths))]
+                             (format fmt (str col)))))
+         right)))
+
+(defn max-width [rows key]
+  (apply max
+         (count (str key))
+         (map #(count (str (get % key))) rows)))
+
+(defn pretty-print-table
+  [rows with-extra-seperator-line log-fn]
+  (let [keys (->> (map keys rows)
+                  (distinct)
+                  (reduce concat)
+                  (distinct))
+        widths (map (partial max-width rows) keys)
+        spacers (map #(apply str (repeat % "-")) widths)
+        formatter (get-formatter keys widths)]
+    (when (seq rows)
+      (log-fn "\n")
+      (log-fn (formatter "| " " | " " |" (zipmap keys keys)))
+      (log-fn (formatter "|-" "-+-" "-|" (zipmap keys spacers)))
+      (doseq [row rows]
+        (log-fn (formatter "| " " | " " |" row))
+        (if with-extra-seperator-line
+          (log-fn (formatter "|-" "---" "-|" (zipmap keys spacers)))))
+      (log-fn "\n"))))
 
 (defn print-compact-table [rows]
-  (print-table rows false m/info))
+  (pretty-print-table rows false m/info))
 
 (defn print-full-table [rows]
-  (print-table rows true m/info))
+  (pretty-print-table rows true m/info))
