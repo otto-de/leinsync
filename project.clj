9dcf282a4bac25eae7e7a7ce2c044e2f12b1099b
Minh Tuan Nguyen <minhtuannguyen2704@gmail.com>
2018-09-13 13:16:24 +0200

    update dependencies

--- project.clj
+++ project.clj
@@ -10,16 +10,16 @@
 
   :dependencies [[org.clojure/math.combinatorics "0.1.4"]
                  [com.sun.jna/jna "3.0.9"]
-                 [com.github.jnr/jnr-posix "3.0.45"]
+                 [com.github.jnr/jnr-posix "3.0.46"]
                  [digest "1.4.8"]
                  [ancient-clj "0.6.15"]
                  [slingshot "0.12.2"]
-                 [org.clojure/tools.cli "0.3.7"]]
+                 [org.clojure/tools.cli "0.4.0"]]
 
   :profiles {:uberjar {:aot :all}
              :test    {:resource-paths ["test-resources"]}
              :dev     {:dependencies [[pjstadig/humane-test-output "0.8.3"]]
-                       :plugins      [[lein-cljfmt "0.5.7"]
-                                      [lein-cloverage "1.0.10"]
-                                      [jonase/eastwood "0.2.6"]
+                       :plugins      [[lein-cljfmt "0.6.0"]
+                                      [lein-cloverage "1.0.13"]
+                                      [jonase/eastwood "0.2.9"]
                                       [lein-kibit "0.1.6"]]}})
