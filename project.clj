744bab6d59cf2a17a2c9f613f75410a9bc6d7152
Minh Tuan Nguyen <minhtuannguyen2704@gmail.com>
2019-03-18 19:30:48 +0100

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject sync "0.9.49"
+(defproject sync "0.9.50-SNAPSHOT"
   :description "sync is a Leiningen plugin to sync same code base between different clojure projects"
   :url "https://github.com/otto-de/leinsync"
   :license {:name "Apache License 2.0"
